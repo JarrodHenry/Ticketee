63db1ffb3e21434b969ca672bf10cdbf9af108a4
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-07 14:15:29 -0500

    Tickets can now be edited

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -21,6 +21,20 @@ class TicketsController < ApplicationController
 	def show
 	end
 
+	def edit
+	end
+
+	def update
+		if @ticket.update(ticket_params)
+			flash[:notice] = "Ticket has been updated."
+			redirect_to [@project, @ticket]
+		else
+			flash.now[:alert] = "Ticket has not been updated."
+			render "edit"
+		end
+	end
+	
+
 	private
 	def set_project
 		@project = Project.find(params[:project_id])
