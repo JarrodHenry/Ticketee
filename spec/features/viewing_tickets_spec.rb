b4ff6e8046974493ff7f41d5d28a8f5bf38d7a34
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-07 18:42:16 -0500

    Link tickets and users upon ticket creation

--- spec/features/viewing_tickets_spec.rb
+++ spec/features/viewing_tickets_spec.rb
@@ -2,12 +2,13 @@ require "rails_helper"
 
 RSpec.feature "Users can view tickets" do
 	before do
+		author = FactoryGirl.create(:user)
 		sublime = FactoryGirl.create(:project, name: "Sublime Text 3")
-		FactoryGirl.create(:ticket, project: sublime, name: "Make it shiny!", 
+		FactoryGirl.create(:ticket, project: sublime, name: "Make it shiny!", author: author,  
 			description: "Gradients! Starbursts! Oh my!")
 
 		ie = FactoryGirl.create(:project, name: "Internet Explorer")
-		FactoryGirl.create(:ticket, project: ie, name: "Standards compliance", description: "Isn't a joke")
+		FactoryGirl.create(:ticket, project: ie, name: "Standards compliance", author: author , description: "Isn't a joke")
 
 		visit "/"
 	end
