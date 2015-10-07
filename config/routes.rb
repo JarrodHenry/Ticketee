ca9dc2544699ea752d660b5f851ad1662a554925
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-07 12:44:57 -0500

    Implement creating tickets for a project

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'projects#index'
-   resources :projects
+   resources :projects do
+	resources :tickets
+   end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
