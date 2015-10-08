c9cb48d8692ae1fd17114997269c414ccfd1d3eb
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-08 18:51:23 -0500

    Only admins can see the links to create and delete projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
 	def title(*parts)
-		unless parts.empty?	
-			content_for :title do	
+		unless parts.empty?
+			content_for :title do
 				(parts << "Ticketee").join(" - ")
 			end
 		end
 	end
-	
+
+	def admins_only(&block)
+		block.call if current_user.try(:admin?)
+	end
+
+
 end
