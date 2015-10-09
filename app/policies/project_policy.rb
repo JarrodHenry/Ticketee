e225ac1ca034927dfaa46194cc2e263fc3fe0467
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-09 18:57:16 -0500

    Don't list projects that a user doesn't have permission to see

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,10 +1,14 @@
 class ProjectPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope
+      return scope.none if user.nil?
+      return scope.all if user.admin?
+
+      scope.joins(:roles).where(roles: {user_id: user})
     end
 
   end
+
 
   def show?
     user.try(:admin?) || record.roles.exists?(user_id: user)
