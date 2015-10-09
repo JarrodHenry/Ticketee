44aab2ed6d317b1bb678d9210fcb6098d497da62
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-09 18:45:10 -0500

    Handle authorization errors gracefully

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -6,7 +6,17 @@ RSpec.describe ProjectsController, type: :controller do
 	expect(response).to redirect_to(projects_path)
 
 	message = "The project you were looking for could not be found."
-	expect(flash[:alert]).to eq message	
+	expect(flash[:alert]).to eq message
    end
 
+   it "handles permission errors by redirecting to a safe place" do
+     allow(controller).to receive(:current_user)
+
+     project = FactoryGirl.create(:project)
+     get :show, id: project
+
+     expect(response).to redirect_to(root_path)
+     message = "You aren't allowed to do that."
+     expect(flash[:alert]).to eq message
+   end
 end
