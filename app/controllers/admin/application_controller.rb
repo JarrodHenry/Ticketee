f9d08aec1fb9f1e6c11657699a89df625961fdd1
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-12 12:49:36 -0500

    Verify that we use authorization in all controllers

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ApplicationController < ApplicationController
+  skip_after_action :verify_authorized, :verify_policy_scoped
   before_action :authorize_admin!
+
   def index
   end
 
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,7 @@ class ProjectsController < ApplicationController
 	end
 
 	def edit
+		authorize @project, :edit?
 	end
 
 	def update
