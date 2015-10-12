18efa83479b8f5e907c4cbb333750ec7d2051d0f
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-12 11:46:41 -0500

    Added permissions for editing tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -11,7 +11,7 @@ class TicketsController < ApplicationController
 		@ticket = @project.tickets.build(ticket_params)
 		@ticket.author = current_user
 		authorize @ticket, :create?
-		
+
 		if @ticket.save
 			flash[:notice] = "Ticket has been created."
 			redirect_to [@project, @ticket]
@@ -26,9 +26,12 @@ class TicketsController < ApplicationController
 	end
 
 	def edit
+		authorize @ticket, :update?
 	end
 
 	def update
+		authorize @ticket, :update?
+		
 		if @ticket.update(ticket_params)
 			flash[:notice] = "Ticket has been updated."
 			redirect_to [@project, @ticket]
--- spec/features/editing_tickets_spec.rb
+++ spec/features/editing_tickets_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "Users can edit existing tickets" do
 		 FactoryGirl.create(:ticket, project:project, author: author)
 	end
 	before do
-		assign_role!(author, :viewer, project)
+		assign_role!(author, :editor, project)
 		login_as(author)
 		visit project_ticket_path(project,ticket)
 		click_link "Edit Ticket"
