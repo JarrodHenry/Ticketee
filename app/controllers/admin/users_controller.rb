650b23ffbcfecbabfc33ea784cef476bf235952c
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-12 20:34:40 -0500

    Bugfixing from last build

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -52,13 +52,6 @@ before_action :set_user, only: [:show, :edit, :update, :archive]
       @user.roles.clear
       build_roles_for(@user)
 
-      role_data = params.fetch(:roles, [])
-      role_data.each do |project_id, role_name|
-        if role_name.present?
-          @user.roles.build(project_id: project_id, role: role_name)
-        end
-      end
-
       if @user.update(user_params)
         flash[:notice] = "User has been updated."
         redirect_to admin_users_path
@@ -92,6 +85,6 @@ before_action :set_user, only: [:show, :edit, :update, :archive]
         end
       end
     end
-    
+
 
 end
