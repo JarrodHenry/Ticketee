6f04b83b2430911c54966bd0e930d3eac81b11df
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-15 10:51:18 -0500

    Auto-assign the default state to newly-created tickets

--- app/models/state.rb
+++ app/models/state.rb
@@ -3,6 +3,10 @@ class State < ActiveRecord::Base
     State.update_all(default:false)
     update!(default: true)
   end
+
+  def self.default
+    find_by(default: true)
+  end
   
   def to_s
     name
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -7,7 +7,15 @@ class Ticket < ActiveRecord::Base
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
   validates :name, presence: true
   validates :description, presence: true, length: {minimum: 10 }
+  before_create :assign_default_state
 
   def attachment_changed?
 	end
+
+  private
+
+  def assign_default_state
+    self.state ||= State.default
+  end
+  
 end
