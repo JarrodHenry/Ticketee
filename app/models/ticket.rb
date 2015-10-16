0ede442dd61b302033af55fe2ad3cf04aad0d561
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-16 11:37:59 -0500

    Users can tag tickets upon creation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,18 +4,31 @@ class Ticket < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   has_many :attachments, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_and_belongs_to_many :tags, uniq: true
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
   validates :name, presence: true
   validates :description, presence: true, length: {minimum: 10 }
   before_create :assign_default_state
-
+  attr_accessor :tag_names
   def attachment_changed?
 	end
+
+  def tag_names
+    @tag_names
+  end
+
+  def tag_names=(names)
+    @tag_names = names
+    names.split.each do |name|
+      self.tags << Tag.find_or_initialize_by(name: name)
+    end
+  end
 
   private
 
   def assign_default_state
     self.state ||= State.default
   end
-  
+
+
 end
--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -84,4 +84,18 @@ click_button "Create Ticket"
 		end
 	end
 
+	scenario "with associated tags" do
+		fill_in "Name", with: "Non-standards compliance"
+		fill_in "Description", with: "My pages are ugly!"
+		fill_in "Tags", with: "browser visual"
+
+		click_button "Create Ticket"
+
+		expect(page).to have_content "Ticket has been created."
+		within ("#ticket #tags") do
+			expect(page).to have_content "browser"
+			expect(page).to have_content "visual"
+		end
+	end
+
 end
