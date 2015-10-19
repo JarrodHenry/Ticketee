af5c9639b7057f12ff3f6419725ad81e7a77dcf9
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-19 09:48:05 -0500

    Code error in initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       provider: "AWS",
-      aws_access_key_id: ENV["S3_KEY"]
+      aws_access_key_id: ENV["S3_KEY"],
       aws_secret_access_key: ENV["S3_SECRET"],
       region: ENV["S3_REGION"]
     }
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   else
     config.storage :file
   end
-  
+
 end
