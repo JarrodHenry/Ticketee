03c8e4f8dca93106ed0dc6123018f24cce2a4aa3
Jarrod Henry <jarrodhenry@gmail.com>
2015-10-28 11:44:18 -0500

    Testing beautify of controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,25 +2,24 @@ class CommentsController < ApplicationController
   before_action :set_ticket
 
   def create
-
     @creator = CommentCreator.build(@ticket.comments, current_user,
-      sanitized_parameters)
+                                    sanitized_parameters)
 
     authorize @creator.comment, :create?
 
     if @creator.save
-      flash[:notice] = "Comment has been created."
+      flash[:notice] = 'Comment has been created.'
       redirect_to [@ticket.project, @ticket]
     else
-      flash.now[:alert] = "Comment has not been created."
+      flash.now[:alert] = 'Comment has not been created.'
       @project = @ticket.project
       @comment = @creator.comment
-      render "tickets/show"
+      render 'tickets/show'
     end
   end
 
-
   private
+
   def set_ticket
     @ticket = Ticket.find(params[:ticket_id])
   end
@@ -30,17 +29,12 @@ class CommentsController < ApplicationController
   end
 
   def sanitized_parameters
-    whitelisted_params =comment_params
+    whitelisted_params = comment_params
 
-    unless policy(@ticket).change_state?
-      whitelisted_params.delete(:state_id)
-    end
+    whitelisted_params.delete(:state_id) unless policy(@ticket).change_state?
 
-    unless policy(@ticket).tag?
-      whitelisted_params.delete(:tag_names)
-    end
+    whitelisted_params.delete(:tag_names) unless policy(@ticket).tag?
 
     whitelisted_params
   end
-
 end
